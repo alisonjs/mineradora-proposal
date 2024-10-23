df3173e995be14c2ca93c784c3ae9e1c884a137a
Alison Silva <alison.dev.silva@gmail.com>
2024-10-23 00:53:33 -0300

    Fix configurations

--- src/main/java/org/br/mineradora/controller/ProposalController.java
+++ src/main/java/org/br/mineradora/controller/ProposalController.java
@@ -1,27 +1,35 @@
 package org.br.mineradora.controller;
 
+import io.quarkus.security.Authenticated;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.br.mineradora.dto.ProposalDetailsDTO;
 import org.br.mineradora.service.ProposalService;
+import org.eclipse.microprofile.jwt.JsonWebToken;
 
 @Path("/api/proposal")
 @Slf4j
+@Authenticated
 public class ProposalController {
 
     @Inject
     ProposalService proposalService;
 
+    @Inject
+    JsonWebToken jsonWebToken;
+
     @GET
     @Path("/{id}")
-    //@RolesAllowed({"user", "manager"})
+    @RolesAllowed({"user", "manager"})
     public ProposalDetailsDTO findDetailsProposal(@PathParam("id") long id){
         return proposalService.findFullProposal(id);
     }
 
     @POST
+    @RolesAllowed({"proposal-customer"})
     public Response createNewProposal(ProposalDetailsDTO proposalDetailsDTO){
         log.info("-- Receiving new proposal --");
         try {
@@ -34,6 +42,7 @@ public class ProposalController {
 
     @DELETE
     @Path("/{id}")
+    @RolesAllowed({"manager"})
     public Response removeProposal(@PathParam("id") long id){
         try {
             proposalService.removeProposal(id);
